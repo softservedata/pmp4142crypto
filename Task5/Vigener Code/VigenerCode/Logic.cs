385def431fd9850b36661ff6095b5dcfb8ee057b
IvanVynyavsky <ivanvynyavsky@gmail.com>
2021-03-25 12:40:49 +0200

    Vigener Cipher, Decipher

---
+++ Task5/Vigener Code/VigenerCode/Logic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VigenerCode
+{
+    public class Logic
+    {
+        public string Encipher(string plainText, string key)
+        {
+            string cipherText = string.Empty;
+
+            int j = 0;
+            for(int i = 0; i < plainText.Length; i++)
+            {
+                cipherText += encipher(plainText[i], key[j]);
+                if (j == key.Length - 1)
+                    j = 0;
+                else
+                    j++;
+            }
+            return cipherText;
+        }
+        public string Decipher(string cipherText, string key)
+        {
+            string plainText = string.Empty;
+            int j = 0;
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                plainText += decipher(cipherText[i], key[j]);
+                if (j == key.Length - 1)
+                    j = 0;
+                else
+                    j++;
+            }
+            return plainText;
+        }
+        public char encipher(char ch, char key)
+        {
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((ch+key-2*d)%26+d);
+        }
+        public char decipher(char ch, char key)
+        {
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((ch - key + 26) % 26 + d);
+        }
+
+    }
+}
---
+++ Task5/Vigener Code/VigenerCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VigenerCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic logic = new Logic();
+            Console.WriteLine("Encode : " + logic.Encipher("ATTACKATDAWN", "LEMON"));
+            Console.WriteLine("Encode : " + logic.Encipher("attackatdawn", "lemon"));
+            Console.WriteLine("Decode : " + logic.Decipher(logic.Encipher("ATTACKATDAWN", "LEMON"), "LEMON"));
+            Console.WriteLine("Decode : " + logic.Decipher(logic.Encipher("attackatdawn", "lemon"), "lemon"));
+        }
+    }
+}
